2e4a466345dc82bb57bc70847810a5202e556c7c
dmsansan <562404326@qq.com>
2019-06-24 15:59:45 +0800

    添加微服务项目结构代码

---
+++ eureka-server/src/main/java/server/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAutoConfiguration
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}

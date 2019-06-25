a14570a7a22e41eed09282f80637941c7548dc49
dmsansan <562404326@qq.com>
2019-06-25 09:48:42 +0800

    添加负载均衡服务

---
+++ ribbon-server/src/main/java/server/ribbon/RibbonServerApplication.java
@@ -0,0 +1,13 @@
+package server.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonServerApplication.class, args);
+    }
+
+}

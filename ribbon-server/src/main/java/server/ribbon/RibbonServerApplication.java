cf01d6f21a04d8b05cef5f1252ef1e9891213ab5
dmsansan <562404326@qq.com>
2019-06-25 10:34:36 +0800

    修改springBoot的启动banner预览图

--- ribbon-server/src/main/java/server/ribbon/RibbonServerApplication.java
+++ ribbon-server/src/main/java/server/ribbon/RibbonServerApplication.java
@@ -2,8 +2,10 @@ package server.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RibbonServerApplication {
 
     public static void main(String[] args) {
---
+++ ribbon-server/src/main/java/server/ribbon/route/RouteTest.java
@@ -0,0 +1,8 @@
+package server.ribbon.route;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouteTest {
+    
+}

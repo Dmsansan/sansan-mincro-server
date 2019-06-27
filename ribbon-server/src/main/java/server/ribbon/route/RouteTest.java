16a1e4f624a50c2d18498575d053b35b95f161cc
dmsansan <562404326@qq.com>
2019-06-27 14:11:37 +0800

    添加hystrix熔断服务器机制

--- ribbon-server/src/main/java/server/ribbon/route/RouteTest.java
+++ ribbon-server/src/main/java/server/ribbon/route/RouteTest.java
@@ -1,5 +1,6 @@
 package server.ribbon.route;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +14,15 @@ public class RouteTest {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     @RequestMapping(value = "/getUserInfo", method = RequestMethod.GET, produces = "application/json")
     public String getUserInfo(){
         String url = "http://PRODUCE-SERVER/user/getUserInfo";
         String userInfo = restTemplate.getForObject(url, String.class);
         return userInfo;
+    }
+
+    public String hiError(){
+        return "request server error";
     }
 }

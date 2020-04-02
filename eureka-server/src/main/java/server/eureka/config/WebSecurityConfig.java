c97256af4d0a76e99a5da8032d6e0b76c527d645
dmsansan <562404326@qq.com>
2020-04-02 15:07:54 +0800

    client添加账号密码授权认证

--- eureka-server/src/main/java/server/eureka/config/WebSecurityConfig.java
+++ eureka-server/src/main/java/server/eureka/config/WebSecurityConfig.java
@@ -2,7 +2,10 @@ package server.eureka.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import server.eureka.security.CustomerPasswordEncode;
 
@@ -11,10 +14,21 @@ import server.eureka.security.CustomerPasswordEncode;
  * @date 2020/4/2  13:23
  */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new CustomerPasswordEncode();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        // 如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 }

db2b248743f19f3ac8c63d6def86b5cbec2ac02d
dmsansan <562404326@qq.com>
2020-04-04 21:54:11 +0800

    1、完善produce2和ribbon注册日志配置信息; 2、修改eureka以ip注册并且显示IP信息。

---
+++ ribbon-server/src/main/java/server/ribbon/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package server.ribbon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author siss
+ * @date 2020/4/4  21:12
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}

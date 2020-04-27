be9d1c66bda7920b84a4684e54fa06ce6ea82a21
dmsansan <562404326@qq.com>
2020-04-27 10:56:50 +0800

    mybatis-plus分页查询，用户addUser

--- system-server/src/main/java/com/sansan/server/user/controller/MrUserInfoController.java
+++ system-server/src/main/java/com/sansan/server/user/controller/MrUserInfoController.java
@@ -1,17 +1,21 @@
 package com.sansan.server.user.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sansan.server.user.domain.entity.MrUserInfo;
 import com.sansan.server.user.service.MrUserInfoService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -33,5 +37,49 @@ public class MrUserInfoController {
     public ResponseEntity<MrUserInfo> queryUserInfo(){
         MrUserInfo userInfo = mrUserInfoService.getById(1);
         return new ResponseEntity<>(userInfo, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/queryUserInfoByPage", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<IPage<MrUserInfo>> queryUserInfoByPage(@RequestParam(value = "curPage") int curPage,
+                                                                 @RequestParam(value = "pageNum") int pageNum,
+                                                                 @RequestBody MrUserInfo mrUserInfo){
+        QueryWrapper<MrUserInfo> queryWrapper = new QueryWrapper<>();
+        if(mrUserInfo.getUserCode()!=null)queryWrapper.like("user_code", mrUserInfo.getUserCode());
+        if(mrUserInfo.getUserName()!=null)queryWrapper.like("user_name",mrUserInfo.getUserName());
+
+        Page<MrUserInfo> page = new Page<>(curPage, pageNum);
+        IPage<MrUserInfo> result = mrUserInfoService.page(page, queryWrapper);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/addUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addUser(@RequestBody MrUserInfo mrUserInfo){
+        ResponseEntity<?> responseEntity = null;
+        if(StringUtils.isEmpty(mrUserInfo.getUserCode())||StringUtils.isEmpty(mrUserInfo.getUserName())||
+                StringUtils.isEmpty(mrUserInfo.getPassWord())){
+            responseEntity = new ResponseEntity<>("缺少必要参数", HttpStatus.NOT_ACCEPTABLE);
+            return responseEntity;
+        }
+        QueryWrapper<MrUserInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_code", mrUserInfo.getUserCode());
+        queryWrapper.or();
+        queryWrapper.eq("user_name", mrUserInfo.getUserName());
+
+        List<Object> userInfo = mrUserInfoService.listObjs(queryWrapper);
+        if(userInfo.size()>0){
+            responseEntity = new ResponseEntity<>("用户已经存在", HttpStatus.FOUND);
+            return responseEntity;
+        }
+        mrUserInfo.setCreateTime(LocalDateTime.now());
+        boolean save = mrUserInfoService.save(mrUserInfo);
+        if(save){
+            responseEntity = new ResponseEntity<>("用户添加成功", HttpStatus.OK);
+        }else {
+            responseEntity = new ResponseEntity<>("用户添加失败", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
     }
 }
--- system-server/src/test/java/com/sansan/server/SystemServerApplicationTests.java
+++ system-server/src/test/java/com/sansan/server/SystemServerApplicationTests.java
@@ -1,16 +1,47 @@
 package com.sansan.server;
 
+import com.sansan.server.user.domain.entity.MrUserInfo;
+import com.sansan.server.user.service.MrUserInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SystemServerApplicationTests {
+
+    @Autowired
+    private MrUserInfoService userInfoService;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void getUserInfo(){
+        MrUserInfo userInfo = userInfoService.getById(1);
+        log.info("测试用户信息：{}", userInfo);
+    }
+
+    @Test
+    public void insertUserInfo(){
+        MrUserInfo userInfo = new MrUserInfo();
+        userInfo.setUserCode("test");
+        userInfo.setUserName("test");
+        userInfo.setPassWord("test");
+        userInfo.setToken("9e405308be6d4df24d21f6b7548bd40327df0ce6");
+        userInfo.setCreateTime(LocalDateTime.now());
+        for (int i = 0; i < 15; i++) {
+            userInfoService.save(userInfo);
+        }
     }
 
 }
